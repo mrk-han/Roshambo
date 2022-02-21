1e43ed4a279b930e07618a98cd0bd72e076db944
mrk-han <mark.han@willowtreeapps.com>
2022-02-21 12:18:19 -0500

    finish UI for initial ViewController   - stub code for presenting ResultsViewController

---
+++ Roshambo/ResultsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultsViewController.swift
+//  Roshambo
+//
+//  Created by Mark Han on 2/20/22.
+//
+
+import Foundation
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet var resultsImage: UIImage!
+    @IBOutlet var resultsLabel: UILabel!
+    @IBOutlet var playAgainButton: UIButton!
+    var gameResult: Bool!
+    
+//    switch gameResults {
+//    case isRock:
+//        print("Rock")
+//    case isPaper:
+//        print("Paper")
+//    case isScissors:
+//        print("Scissors")
+//    default:
+//        "This should never happen"
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- Roshambo/ViewController.swift
+++ Roshambo/ViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var rockButton: UIButton!
+    @IBOutlet var paperButton: UIButton!
+    @IBOutlet var scissorsButton: UIButton!
+    @IBOutlet var letsPlayLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func pickRock() {
+        var rockController: ResultsViewController
+        rockController = storyboard?.instantiateViewController(withIdentifier: "ResultsController") as! ResultsViewController
+        rockController.resultsImage = nil
+        rockController.resultsLabel = nil
+        rockController.playAgainButton = nil
+        
+        present(rockController, animated: true, completion: nil)
+        
+    }
+    
 
 
 }
